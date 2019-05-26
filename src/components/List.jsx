54a64bac5344cb1d3bae4d74e2a52dfc2f1b3946
Rafael Sergio <rafa.sergio.02@live.com>
2019-05-26 15:37:52 -0300

    add margin

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,7 +5,7 @@ const List = (props) => (
     <li className="lista__item">
         <div style={{width: "100%", display: "flex", justifyContent: "space-between"}}>
             <input type="checkbox" name="check" id="ck" checked={props.done} onChange={() => {props.check(props.task)}}/>
-            <span style={{textDecoration: `${props.done ? 'line-through' : 'none'}`}}>{props.task}</span>
+            <span style={{textDecoration: `${props.done ? 'line-through' : 'none'}`, marginLeft: 10, marginRight: 10}}>{props.task}</span>
             <button style={{border: '1px solid black', borderRadius: 10, padding:10,}} onClick={() => {props.remove(props.task)}}>Delete</button>
         </div>
     </li>
